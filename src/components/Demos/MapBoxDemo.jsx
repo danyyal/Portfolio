a60a53006d5a3af891b37eb8724f2f322cbd12f4
Danyyal ali yar <49082161+danyyal@users.noreply.github.com>
2025-08-12 15:34:03 +0500

    Add files via upload

--- src/components/Demos/MapBoxDemo.jsx
+++ src/components/Demos/MapBoxDemo.jsx
@@ -1,36 +1,84 @@
-// ES6
-import ReactMapboxGl, { Layer, Feature, ZoomControl,ScaleControl } from 'react-mapbox-gl';
-import 'mapbox-gl/dist/mapbox-gl.css';
-import { Container } from 'react-bootstrap';
- 
-const Map = ReactMapboxGl({
-  accessToken:
-    'pk.eyJ1IjoiZGFueXlhbCIsImEiOiJjbWF6MXluZXYwZDQxMmpzaGhmODFseHJiIn0.qZWNaS6U03CBBA7Cm-aGYg',
-    keyboard: true,
-   
-});
+import React, { useEffect, useRef } from 'react';
+// import mapboxgl from 'mapbox-gl';
+// import 'mapbox-gl/dist/mapbox-gl.css';
+// import Map from 'react-map-gl';
 
-const MapBoxDemo = () => (
-   <Map
-    style="mapbox://styles/mapbox/streets-v9"
-    containerStyle={{
-      height: '400px',
-      width: '400px'
-    }}
-    renderWorldCopies
-    animationOptions={{
-      animate: true,
-      duration: 1000,
-    }}
+// mapboxgl.accessToken = 'pk.eyJ1IjoiZGFueXlhbCIsImEiOiJjbWF6MXluZXYwZDQxMmpzaGhmODFseHJiIn0.qZWNaS6U03CBBA7Cm-aGYg';
 
-  >
-    <Layer type="symbol" id="marker" layout={{ 'icon-image': 'marker-15' }}>
-      <Feature coordinates={[-0.481747846041145, 51.3233379650232]} />
-      <ZoomControl/>
-      <ScaleControl/>
+const MapBoxDemo = () => {
+  const mapContainer = useRef(null);
+  const map = useRef(null);
 
-    </Layer>
-  </Map>
-);
+  // useEffect(() => {
+  //   if (map.current) return; // Initialize map only once
+
+  //   map.current = new mapboxgl.Map({
+  //     container: mapContainer.current,
+  //     style: 'mapbox://styles/mapbox/streets-v11',
+  //     center: [74.253564, 31.479281],
+  //     zoom: 15.5,
+  //     pitch: 45,
+  //     bearing: -17.6,
+  //     antialias: true // For smoother 3D rendering
+  //   });
+
+  //   map.current.on('load', () => {
+  //     // Add 3D buildings layer
+  //     map.current.addLayer({
+  //       id: '3d-buildings',
+  //       source: 'composite',
+  //       'source-layer': 'building',
+  //       filter: ['==', 'extrude', 'true'],
+  //       type: 'fill-extrusion',
+  //       minzoom: 15,
+  //       paint: {
+  //         'fill-extrusion-color': '#aaa',
+  //         'fill-extrusion-height': [
+  //           'interpolate',
+  //           ['linear'],
+  //           ['zoom'],
+  //           15, 0,
+  //           15.05, ['get', 'height']
+  //         ],
+  //         'fill-extrusion-base': [
+  //           'interpolate',
+  //           ['linear'],
+  //           ['zoom'],
+  //           15, 0,
+  //           15.05, ['get', 'min_height']
+  //         ],
+  //         'fill-extrusion-opacity': 0.6
+  //       }
+  //     });
+
+  //     // Add a marker
+  //     new mapboxgl.Marker()
+  //       .setLngLat([74.253564, 31.479281])
+  //       .addTo(map.current);
+
+  //     // Add navigation controls
+  //     map.current.addControl(new mapboxgl.NavigationControl());
+  //     map.current.addControl(new mapboxgl.ScaleControl());
+  //   });
+
+  //   // Clean up on unmount
+  //   return () => {
+  //     if (map.current) {
+  //       map.current.remove();
+  //       map.current = null;
+  //     }
+  //   };
+  // }, []);
+
+  return (
+    <div
+      // ref={mapContainer}
+      style={{
+        height: '400px',
+        width: '400px'
+      }}
+    />
+  );
+};
 
 export default MapBoxDemo;
